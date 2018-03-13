6c60e7d83f1fe939dee2002a00e013418df22d30
BlackCat06 <em_hammi@esi.dz>
2018-03-13 19:52:18 +0100

    some CSS

--- FrontEndMobile_IHM/app/shared/user/user.ts
+++ FrontEndMobile_IHM/app/shared/user/user.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from "rxjs/BehaviorSubject";
 export class User {
     id : number;
     email: string;
+    passwordConfirm : string;
     password: string;
     firstname: string;
     lastname: string;
